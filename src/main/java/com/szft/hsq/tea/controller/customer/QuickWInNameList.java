d64808a54a3956a8ce721b789fd331388d1bbd93
coffee <Administrator@zhongjiahui>
2019-05-14 00:05:38 +0800

    SR #7980 创建快赢名单 新增

---
+++ src/main/java/com/szft/hsq/tea/controller/customer/QuickWInNameList.java
@@ -0,0 +1,14 @@
+package com.szft.hsq.tea.controller.customer;
+
+public class QuickWInNameList {
+	private String name ;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
